b632dc2a9aed51c8d1e093286a0c29d815710d4d
Gnlow <gnlow@protonmail.com>
2023-07-02 17:04:38 +0900

    Update od.ts

--- test/od.ts
+++ test/od.ts
@@ -1,48 +1,12 @@
-const count =
-    (str: string) =>
-    (arr: string[]) =>
-    arr.filter(x => x == str).length
+import {
+    Permutation
+} from "npm:js-combinatorics"
 
-const pp =
-    (curr: string, remain: string[]): string[] =>
-    remain.length == 0
-        ? [curr]
-        : remain
-        .flatMap(
-            (x, i, l) => {
-                if (x == "x") {
-                    if (curr == "") return []
-                }
-                if (x == ")") {
-                    if (count("(")(remain) == count(")")(remain)) {
-                        return []
-                    }
-                }
-                return pp(
-                    curr + x,
-                    l.filter((_, j) => i != j),
-                )
-            }
-        )
-
-const undup =
-    (arr: string[]) =>
-    [...new Set(arr)]
-
-const t =
-    undup(pp("(", "(xx))".split("")))
-    .flatMap(str => {
-        const [a,b,c,d,e,f] = str
-        return [
-            [e+f+a, b+c+d],
-            [f+a+b, c+d+e],
-            [a+b+c, d+e+f],
-        ].map(x => x.join(""))
-    })
+import Iteruyo, { $ } from "https://deno.land/x/iteruyo@v0.3.0/mod.ts"
 
 import { Group } from "../mod.ts"
 
-const set = [...new Set(t)]
+const set = [...new Set($(new Permutation("(())xx")).map(x => x.join("")).toArray())]
 console.log(set)
 
 const od = new Group({
